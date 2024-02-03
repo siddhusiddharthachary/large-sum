7c5a97b5e21a9e4a326bec93586660cbdaabcff8
siddhusiddharthachary <151196424+siddhusiddharthachary@users.noreply.github.com>
2024-02-03 14:15:40 +0530

    Create main.cpp

---
+++ .github/workflows/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int sum=0;
+int sunDIgits(int x){
+  sum = sum+x;
+  return sum;
+}
+int main(){
+  for(int i=0;i<5;i++){
+    cout<<"Enter the number"<<endl;
+  cin>>n;
+  sumDigits(n);
+  }
+  return 0;
+}
